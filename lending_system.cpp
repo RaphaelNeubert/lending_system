c4e363235c53fc69ee2b47efbcc682b57e0afdf1
Raphael Neubert <raphi.neubert@gmail.com>
2021-06-20 20:26:38 +0200

    moves objects into list

--- lending_system.cpp
+++ lending_system.cpp
@@ -19,9 +19,13 @@ void lending_system::read_medium(){
     while (!in.atEnd()) {
         QString line = in.readLine();
         QStringList fields = line.split(";");
-        book m1(fields[0],fields[2],fields[3].toUInt(),fields[4].toInt(),fields[5].toUInt(),QDate::fromString(fields[6]));
-        std::cout<<m1.get_name().toStdString()<<std::endl;
-        std::cout<<m1.get_author().toStdString()<<std::endl;
-        std::cout<<m1.get_lend()<<std::endl;
+        if(fields[2]=="book"){
+            book m1(fields[0],fields[2],fields[3].toUInt(),fields[4].toInt(),fields[5].toUInt(),
+                    QDate::fromString(fields[6]));
+            std::cout<<m1.get_name().toStdString()<<std::endl;
+            std::cout<<m1.get_author().toStdString()<<std::endl;
+            std::cout<<m1.get_lend()<<std::endl;
+            medlist.append((medium*)&m1);
+        }
     }
 }
