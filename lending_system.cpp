889511561d2c4cf4c3cdcd5c8203467d8bda7ad1
Raphael Neubert <raphi.neubert@gmail.com>
2021-06-21 20:55:00 +0200

    fixed read_medium if statement

--- lending_system.cpp
+++ lending_system.cpp
@@ -13,19 +13,23 @@ lending_system::lending_system(){
 void lending_system::read_medium(){
     QFile f("medien.csv");
     if(!f.open(QIODevice::ReadOnly)){
+//TODO error message
         return;
     }
     QTextStream in(&f);
     while (!in.atEnd()) {
         QString line = in.readLine();
         QStringList fields = line.split(";");
-        if(fields[2]=="book"){
-            book m1(fields[0],fields[2],fields[3].toUInt(),fields[4].toInt(),fields[5].toUInt(),
+        if(fields[1]=="book"){
+            book b(fields[0],fields[2],fields[3].toUInt(),fields[4].toInt(),fields[5].toUInt(),
                     QDate::fromString(fields[6]));
-            std::cout<<m1.get_name().toStdString()<<std::endl;
-            std::cout<<m1.get_author().toStdString()<<std::endl;
-            std::cout<<m1.get_lend()<<std::endl;
-            medlist.append((medium*)&m1);
+            medlist.append((medium*)&b);
+
+           /* 
+            std::cout<<medlist[0]->get_name().toStdString()<<std::endl;
+            std::cout<<((book*)medlist[0])->get_author().toStdString()<<std::endl;
+            std::cout<<medlist[0]->get_lend()<<std::endl;
+            */
         }
     }
 }
