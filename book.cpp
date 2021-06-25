cef2ce9f066b3bf03d99068e7c40f3c9cac9e6d8
Raphael Neubert <raphi.neubert@gmail.com>
2021-06-25 14:48:44 +0200

    Added publisher to book class

--- book.cpp
+++ book.cpp
@@ -2,19 +2,25 @@
 #include "book.h"
 
 book::book(QString name) : medium(name, QString("book")){
-    this->author="";
+    //this->author="";
 }
 
 book::book(QString name, QString author) : medium(name, QString("book")){
     this->author=author;
+    this->publisher=publisher;
 }
 
 book::book(QString name, QString author, unsigned int id, bool lend, unsigned int person_id, QDate lend_date) :
     medium(name, QString("book"),id,lend,person_id,lend_date){
     this->author=author;
+    this->publisher=publisher;
 }
 
 QString book::get_author(){
     return author;
+}
+
+QString book::get_publisher(){
+    return publisher;
 }
 
--- book.h
+++ book.h
@@ -7,12 +7,15 @@
 class book : public medium{
     private:
         QString author;
+        QString publisher;
     
     public:
         book(QString name);
         book(QString name, QString author);
-        book(QString name, QString author, unsigned int id, bool lend, unsigned int person_id, QDate lend_date);
+        book(QString name, QString author, QString publisher);
+        book(QString name, QString author, QString publisher unsigned int id, bool lend, unsigned int person_id, QDate lend_date);
         QString get_author();
+        QString get_publisher();
 };
 
 
